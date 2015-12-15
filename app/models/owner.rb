4a2c7740a960a7e2502d8dd2d8e867ea850ddcdb
monsteronfire <monsteronfire@gmail.com>
2015-12-15 16:52:19 +0800

    adds debug lines in owner model

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -8,13 +8,14 @@ class Owner
   end
 
   def countdown
+    Rails.logger.debug 'DEBUG: entering Owner countdown method'
     today = Date.today
     birthday = Date.new(today.year, birthdate.month, birthdate.day)
-
     if birthday > today
       countdown = (birthday - today).to_i
     else
       countdown = (birthday.next_year - today).to_i
     end
   end
+
 end
