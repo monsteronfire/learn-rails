9fc045a6e9bd69f49754f239805d01921c08880e
monsteronfire <monsteronfire@gmail.com>
2016-01-21 01:54:53 +0800

    adds flash playcode

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,9 @@
 class VisitorsController <ApplicationController
   def new
-    Rails.logger.debug 'DEBUG: entering new method'
+    # Rails.logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    # Rails.logger.debug 'DEBUG: Owner name is ' + @owner.name
+    flash.now[:notice] = 'Welcome!'
+    flash.now[:alert] = 'My birthday is soon.'
   end
 end
